68333c03f4a708f9a5e29205142ec94b4d20a136
Anderson Alencar <anderson23alencar@gmail.com>
2023-02-15 13:10:24 -0300

    feat: basics operations

--- exercise1/app/src/main/java/exercise1/App.java
+++ exercise1/app/src/main/java/exercise1/App.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class App {
 //  Faça um algoritmo que receba dois números e exiba o resultado da sua soma;
-    
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+
         System.out.println("Digite o primeiro número: ");
         int firstNumber = reader.nextInt();
         System.out.println("Digite o segundo número: ");
         int secondNumber = reader.nextInt();
-        
+
         System.out.println(
-            "A soma desses dois números é: " +
-            (firstNumber + secondNumber)    
+                "A soma desses dois números é: "
+                + (firstNumber + secondNumber)
         );
     }
 }
